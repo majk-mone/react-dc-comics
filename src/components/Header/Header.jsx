7dcf2614ad5a525ead80719711b3b66919324c8a
majk.mone <mone.majk@gmail.com>
2026-03-24 14:24:11 +0100

    edit  header links id

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,13 @@ export default function Header() {
 		{ id: 1, text: 'CHARACTERS', url: '#', current: false },
 		{ id: 2, text: 'COMICS', url: '#', current: true },
 		{ id: 3, text: 'MOVIES', url: '#', current: false },
-		{ id: 4, text: 'TV', url: '#', current: false },
-		{ id: 4, text: 'GAMES', url: '#', current: false },
-		{ id: 4, text: 'COLLECTIBLES', url: '#', current: false },
-		{ id: 4, text: 'VIDEOS', url: '#', current: false },
-		{ id: 4, text: 'FANS', url: '#', current: false },
-		{ id: 4, text: 'NEWS', url: '#', current: false },
-		{ id: 4, text: 'SHOP', url: '#', current: false },
+		{ id: 5, text: 'TV', url: '#', current: false },
+		{ id: 6, text: 'GAMES', url: '#', current: false },
+		{ id: 7, text: 'COLLECTIBLES', url: '#', current: false },
+		{ id: 8, text: 'VIDEOS', url: '#', current: false },
+		{ id: 9, text: 'FANS', url: '#', current: false },
+		{ id: 10, text: 'NEWS', url: '#', current: false },
+		{ id: 11, text: 'SHOP', url: '#', current: false },
 	]
 
 	return (
